1ce102d19f935de258339537eecdcfa3f773735c
SuprHUlk <ayush123kumar.ak@gmail.com>
2022-12-23 16:16:55 +0530

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tic-tac-toe
+Live Demo -> [Click](https://suprhulk.github.io/tic-tac-toe/)<br>
+<br>
+Play the classic game of Tic Tac Toe with your friend.
--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const close=document.querySelector(".close");
 const ok=document.querySelector(".submit");
 
 start.addEventListener('click', ()=> {
+    start.remove();
     playerCreation.showModal();
-    close.addEventListener('click', ()=> {
-        playerCreation.close();
+});
+
+close.addEventListener('click', ()=> {
+    playerCreation.close();
+    const btn=document.createElement("button");
+    btn.classList.add("start");
+    btn.textContent="PLAY";
+    mainSelector.insertBefore(btn, document.querySelector("dialog"));
+    btn.addEventListener('click', ()=> {
+        btn.remove();
+        playerCreation.showModal();
     });
 });
 
@@ -20,10 +30,36 @@ const board = (()=> {
 })();
 
 const players = (name, sign)=> {
-    const getName=()=> name;
-    const getSign=()=> sign;
-    return{};
+    const setName=()=> name;
+    const setSign=()=> sign;
+    const getName = name;
+    const getSign = sign;
+    return{getName, getSign};
 }
+
+const renderPlayerInfo=(()=> {
+    let render=()=> {
+        document.querySelector(".player-1 h1").textContent="PLAYER ONE";
+        document.querySelector(".player-1 .name").textContent="Name: "+player1.getName;
+        document.querySelector(".player-1 .sign").textContent="Sign: "+player1.getSign;
+        document.querySelector(".player-2 h1").textContent="PLAYER TWO";
+        document.querySelector(".player-2 .name").textContent="Name: "+player2.getName;
+        document.querySelector(".player-2 .sign").textContent="Sign: "+player2.getSign;
+        document.querySelector(".content").style.gap="10rem";
+        document.querySelector(".player-info").style.background="rgba(255, 255, 255, 0.34)";
+        document.querySelector(".player-info").style.boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)";
+        document.querySelector(".result h1").textContent="RESULT";
+        document.querySelector(".result").style.background="rgba(255, 255, 255, 0.34)";
+        document.querySelector(".result").style.boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)";
+        document.querySelector(".result").style.padding="10px 80px";
+        const reset=document.createElement("button");
+        reset.classList.add("reset");
+        reset.textContent="RESET";
+        reset.addEventListener('click', restart);
+        document.querySelector(".result").appendChild(reset);
+    }
+    return{render};
+})();
 
 const renderBoard = (()=> {
     let render =()=> {
@@ -32,7 +68,7 @@ const renderBoard = (()=> {
         }
         const boardSelector=document.createElement("div");
         boardSelector.classList.add("board");
-        mainSelector.appendChild(boardSelector);
+        document.querySelector(".content").insertBefore(boardSelector, document.querySelector(".result"));
         for(let i=0;i<9;i++) {
             const div=document.createElement("div");
             div.classList.add("block");
@@ -55,15 +91,70 @@ const counterCreator=()=> {
     };
 };
 
+const resultChecker=(()=> {
+    const check=()=> {
+        let c=0;
+        for(let i=1;i<9;i++) {
+            if(i==3||i==6) {
+                c=0;
+            }
+            if(board.gameBoard[i]==board.gameBoard[i-1]&&board.gameBoard[i]!=undefined) {
+                c++;
+            }
+            if(c==2) {
+                return board.gameBoard[i];
+            }
+        }
+        for(let i=0;i<3;i++) {
+            c=0;
+            for(let j=i+3;j<=i+6;j++) {
+                if(board.gameBoard[j]==board.gameBoard[j-3]&&board.gameBoard[j]!=undefined) {
+                    c++;
+                }
+                if(c==2) {
+                    return board.gameBoard[j];
+                }
+            }
+        }
+        c=0;
+        for(let i=4;i<=8;i+=4) {
+            if(board.gameBoard[i]==board.gameBoard[i-4]&&board.gameBoard[i]!=undefined) {
+                c++;
+            }
+            if(c==2) {
+                return board.gameBoard[i];
+            }
+        }
+        c=0;
+        for(let i=4;i<=6;i+=2) {
+            if(board.gameBoard[i]==board.gameBoard[i-2]&&board.gameBoard[i]!=undefined) {
+                c++;
+            }
+            if(c==2) {
+                return board.gameBoard[i];
+            }
+        }
+        return false;
+    }
+    return{check};
+})();
+
 function appendSign() {
     if(board.gameBoard[this.getAttribute("data-attribute")]==undefined) {
         if(counter()%2==0) {
-            board.gameBoard[this.getAttribute("data-attribute")]="X"; 
+            board.gameBoard[this.getAttribute("data-attribute")]=player1.getSign; 
         }
         else {
-            board.gameBoard[this.getAttribute("data-attribute")]="O"; 
+            board.gameBoard[this.getAttribute("data-attribute")]=player2.getSign; 
         }
         renderBoard.render();
+        let flag=resultChecker.check();
+        if(flag==player1.getSign) {
+            document.querySelector(".result h2").textContent=player1.getName+" won";
+        }
+        else if(flag==player2.getSign) {;
+            document.querySelector(".result h2").textContent=player2.getName+" won";
+        }
     }
 }
 
@@ -72,6 +163,19 @@ function createPlayers() {
     player2 = players(document.querySelector("#p2-name").value, document.querySelector("#p2-sign").value);
     counter = counterCreator();
     playerCreation.close();
-    start.remove();
     renderBoard.render();
+    renderPlayerInfo.render();
+}
+
+function restart() {
+    playerCreation.showModal();
+    board.gameBoard=[];
+    renderBoard.render();
+    document.querySelector(".close").addEventListener('click', ()=>{
+        playerCreation.close();
+        if(document.querySelector(".start")!=null) {
+            document.querySelector(".start").remove();
+        }
+    });
+    document.querySelector(".reset").remove();
 }
